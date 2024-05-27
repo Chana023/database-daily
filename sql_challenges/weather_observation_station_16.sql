a9ad80a15df14cf6f94e096eef678467cc3c3814
Chana023 <jesalchana23@gmail.com>
2024-05-27 16:12:22 +0200

    Completed weather observation station 16 sql challenge

---
+++ sql_challenges/weather_observation_station_16.sql
@@ -0,0 +1,3 @@
+-- https://www.hackerrank.com/domains/sql?filters%5Bstatus%5D%5B%5D=unsolved&filters%5Bskills%5D%5B%5D=SQL%20%28Basic%29&badge_type=sql
+
+SELECT ROUND(MIN(LAT_N),4) FROM STATION WHERE LAT_N > 38.7780;
