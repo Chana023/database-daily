c9028b5cb1c7cffa40fbe8b08ff5f032bd416898
Chana023 <jesalchana23@gmail.com>
2025-02-22 16:00:49 +0200

    Completed biggest single number sql challenge

---
+++ leet_code_sql_challenges/619_biggest_single_number.sql
@@ -0,0 +1,10 @@
+-- https://leetcode.com/problems/biggest-single-number/description/
+
+SELECT MAX(num) AS num
+FROM MyNumbers
+WHERE num IN (
+    SELECT num 
+    FROM MyNumbers
+    GROUP BY num
+    HAVING COUNT(num) = 1
+)
