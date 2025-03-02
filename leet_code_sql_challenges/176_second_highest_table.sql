7af118ed4c0f6b317df6bfb3a9aef7b438e4568a
Chana023 <jesalchana23@gmail.com>
2025-03-02 11:34:21 +0200

    Completed 176 with better solution

--- leet_code_sql_challenges/176_second_highest_table.sql
+++ leet_code_sql_challenges/176_second_highest_table.sql
@@ -4,3 +4,6 @@ SELECT (SELECT DISTINCT salary
 FROM employee
 ORDER BY salary DESC
 LIMIT 1 OFFSET 1) as SecondHighestSalary
+
+-- Better solution below
+select max(salary) as SecondHighestSalary from employee where salary < (select max(salary) from employee);
