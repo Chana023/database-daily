fced3cdf470f32f1e54f83e7faf937b2ae18a0b3
Chana023 <jesalchana23@gmail.com>
2025-03-27 16:07:47 +0200

    Completed 3475 DNA pattern recognition sql challenge

---
+++ leet_code_sql_challenges/3475_DNA_pattern_recognition.sql
@@ -0,0 +1,10 @@
+-- https://leetcode.com/problems/dna-pattern-recognition/
+
+SELECT
+    *,
+    dna_sequence REGEXP '^ATG' AS has_start,
+    dna_sequence REGEXP 'TAA$|TAG$|TGA$' AS has_stop,
+    dna_sequence REGEXP 'ATAT' AS has_atat,
+    dna_sequence REGEXP 'GGG' AS has_ggg
+FROM samples
+ORDER BY sample_id
