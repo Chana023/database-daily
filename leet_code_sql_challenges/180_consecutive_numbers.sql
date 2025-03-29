b62e2f6ab76b1ff0913ef38e6e6a54984c89464f
Chana023 <jesalchana23@gmail.com>
2025-03-29 09:32:35 +0200

    Completed 180 consecutive numbers sql challenge

---
+++ leet_code_sql_challenges/180_consecutive_numbers.sql
@@ -0,0 +1,8 @@
+-- https://leetcode.com/problems/consecutive-numbers/description/
+
+select
+    distinct a.Num as ConsecutiveNums 
+from
+    Logs a
+join logs b on a.id=b.id+1 and a.num=b.num
+join logs c on a.id=c.id+2 and a.num=c.num
