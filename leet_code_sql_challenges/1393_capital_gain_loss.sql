7a21a322da2fa11075543e184fbb7ff3fca8acf6
Chana023 <jesalchana23@gmail.com>
2025-03-31 16:22:01 +0200

    Completed 1393 capital gain loss sql challenge

---
+++ leet_code_sql_challenges/1393_capital_gain_loss.sql
@@ -0,0 +1,10 @@
+-- https://leetcode.com/problems/capital-gainloss/description/
+
+SELECT stock_name, SUM(
+    CASE
+        WHEN operation = 'Buy' THEN -price
+        ELSE price
+    END
+) AS capital_gain_loss
+FROM Stocks
+GROUP BY stock_name
