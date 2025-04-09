1300807b5e855e7295c94f8495e3734cb8cdccc1
Chana023 <jesalchana23@gmail.com>
2025-04-09 19:56:30 +0200

    Completed SQL chinook project

---
+++ data_wars/chinook_project_q7.sql
@@ -0,0 +1,4 @@
+-- https://app.datawars.io/project/89a6bd9c-e827-4b8b-81ab-19d5a2adf235?page=4
+
+SELECT CustomerID AS ID, Title, FirstName as 'First Name', LastName as 'Last Name', EmailAddress as Email
+from Customer
