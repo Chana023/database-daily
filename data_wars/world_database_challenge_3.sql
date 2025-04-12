0c18e8284302aa80c8acda839b1c83bc3c4152f1
Chana023 <jesalchana23@gmail.com>
2025-04-12 11:45:08 +0200

    Completed world database sql challenge 3 from datawars

---
+++ data_wars/world_database_challenge_3.sql
@@ -0,0 +1,5 @@
+-- https://app.datawars.io/project/c4417c3d-3630-448d-bdbd-c20cb8327dba?page=2
+
+SELECT Code, Name as 'Country Name', Continent as 'Continent Name', Region, SurfaceArea, IndepYear, Population, LifeExpectancy, GNP, GNPOld, LocalName, GovernmentForm,HeadOfState, Capital, Code2
+
+from country
